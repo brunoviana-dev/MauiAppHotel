7ae0b85055dabe706b3caa595cdb2fd4020b8683
Bruno dos Santos Viana <bruno.viana@hotmail.com>
2025-08-16 10:40:14 -0300

    Continuação da aula 6

--- MauiAppHotel/Views/Contratacao.xaml.cs
+++ MauiAppHotel/Views/Contratacao.xaml.cs
@@ -2,13 +2,26 @@ namespace MauiAppHotel.Views;
 
 public partial class Contratacao : ContentPage
 {
+	App PropriedadesApp;
+
 	public Contratacao()
 	{
 		InitializeComponent();
-	}
+
+		PropriedadesApp = (App)Application.Current;
+
+		pckQuarto.ItemsSource = PropriedadesApp.listaQuartos;
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-
+		try
+		{
+			Navigation.PushAsync(new Contratada());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops!", ex.Message, "Ok");
+		}
     }
 }
